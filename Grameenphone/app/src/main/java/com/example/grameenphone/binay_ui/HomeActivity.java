e174edf82c3f77f2576d69f1d6439a982053099c
binaykumarrana <rajkiran@kavithas-MacBook-Pro-3.local>
2015-09-08 18:28:27 +0530

    Home Screen with side menu

---
+++ Grameenphone/app/src/main/java/com/example/grameenphone/binay_ui/HomeActivity.java
@@ -0,0 +1,31 @@
+package com.example.grameenphone.binay_ui;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.grameenphone.R;
+
+public class HomeActivity extends AppCompatActivity implements FragmentDrawer.FragmentDrawerListener{
+    private Toolbar mToolbar;
+    private FragmentDrawer drawerFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        drawerFragment = (FragmentDrawer)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
+        drawerFragment.setDrawerListener(HomeActivity.this);
+    }
+
+    @Override
+    public void onDrawerItemSelected(View view, int position) {
+
+    }
+}
