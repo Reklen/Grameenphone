1a5db82ca680209336b5f8b6236fe15b294cef58
binaykumarrana <rajkiran@kavithas-MacBook-Pro-3.local>
2015-09-09 10:58:40 +0530

    changes

--- Grameenphone/app/src/main/java/com/example/grameenphone/binay_ui/HomeActivity.java
+++ Grameenphone/app/src/main/java/com/example/grameenphone/binay_ui/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.grameenphone.binay_ui;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -22,10 +25,58 @@ public class HomeActivity extends AppCompatActivity implements FragmentDrawer.Fr
                 getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
         drawerFragment.setDrawerListener(HomeActivity.this);
+        // display the first navigation drawer view on app launch
+        displayView(0);
     }
+
 
     @Override
     public void onDrawerItemSelected(View view, int position) {
+        displayView(position);
+    }
 
+    private void displayView(int position) {
+        Fragment fragment = null;
+        String title = getString(R.string.app_name);
+        switch (position) {
+            case 0:
+                fragment = new HomePage();
+                title = getString(R.string.title_home);
+                break;
+            case 1:
+                fragment = new ProFragment();
+                title = "Pro";
+                break;
+            case 2:
+                fragment = new ManageFavorite();
+                title ="Manage Favorite";
+                break;
+            case 3:
+                fragment = new PinChange();
+                title = "Pin Change";
+                break;
+            case 4:
+                fragment = new DemoFragment();
+                title = "Demo";
+                break;
+            case 5:
+                fragment = new TermsCondition();
+                title = "terms condition";
+                break;
+            case 6:
+                fragment = new Logout();
+                title = "Logout";
+                break;
+            default:
+                break;
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_body, fragment);
+            fragmentTransaction.commit();
+
+        }
     }
 }
---
+++ Grameenphone/app/src/main/java/com/example/grameenphone/binay_ui/HomePage.java
@@ -0,0 +1,45 @@
+package com.example.grameenphone.binay_ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.grameenphone.R;
+
+/**
+ * Created by rajkiran on 09/09/15.
+ */
+public class HomePage extends Fragment {
+    public HomePage() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.home_layout, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
